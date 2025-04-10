858e3471daae55dac4e859bc505c17eb4309bd7b
Satyaki <blackhole12345satyai@gmail.com>
2025-04-10 16:45:37 +0530

    new css styles

--- Frontend/src/app/components/pages/home/home.component.ts
+++ Frontend/src/app/components/pages/home/home.component.ts
@@ -1,7 +1,8 @@
+// Removed duplicate import of sample_foods
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../services/food.service';
-
+import { sample_foods } from '../../../../data';
 @Component({
   selector: 'app-home',
   standalone: false,
@@ -19,5 +20,10 @@ export class HomeComponent {
         this.rating = newRating;
         console.log('New Rating:', this.rating);
         this.foods[index].stars = this.rating;
+        console.log(this.foods[index]);
+
+        sample_foods[index]= this.foods[index];
+        console.log(sample_foods);
+
       }
 }
